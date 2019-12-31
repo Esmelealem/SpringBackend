aebe9b6374e13431d3a78e0955f1523ca4df574d
em@git2019 <eme2119@gmail.com>
2019-12-31 11:53:38 +0300

    First BackEnd

--- src/main/java/com/BookStore/Resource/LoginResource.java
+++ src/main/java/com/BookStore/Resource/LoginResource.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,8 +38,9 @@ public class LoginResource {
 		return new ResponseEntity("Session is Active",HttpStatus.OK);
 		}
 	@RequestMapping(value="/user/logout", method=RequestMethod.POST)
-	public ResponseEntity logOut()
+	public ResponseEntity logout()
 		{
+		SecurityContextHolder.clearContext();
 		return new ResponseEntity("Logout Successfully!",HttpStatus.OK);
 		}
 	}
